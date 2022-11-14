21ead6821de63db0e7985782f8a42104a326aac3
Oghenesuvwe Money <rhounamoney@gmail.com>
2022-11-14 22:53:25 +0100

    Fix: Places section responsive view

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 import styles from '../footer/Footer.module.scss';
+import logo2 from '../../styles/assets/logo2.png';
+import facebook from '../../styles/assets/facebook.png';
+import instagram from '../../styles/assets/instagram.png';
+import twitter from '../../styles/assets/twitter.png';
+import copy from '../../styles/assets/copy.png';
 
 const Footer = () => {
   return (
     <section className={styles.footer}>
-      Footer
+      <div className={styles.footer_links}>
+        <><img className={styles.footer_links__logo} src={logo2} alt="Metabnb logo" /></>
+
+        <div className={styles.footer_links_icon}>
+          <img src={facebook} alt="Facebook icon" />
+          <img src={instagram} alt="Instagram icon" />
+          <img src={twitter} alt="Twitter icon" />
+        </div>
+
+        <p>
+          <img src={copy} alt="" /> 
+          2022 Metabnb
+        </p>
+      </div>
+
+      <div className={styles.footer_note}>
+        <div>
+          <h5>Community</h5>
+          <ul>
+            <li>NFT</li>
+            <li>Tokens</li>
+            <li>Landlords</li>
+            <li>Discord</li>
+          </ul>
+        </div>
+
+        <div>
+          <h5>Places</h5>
+          <ul>
+            <li>Castle</li>
+            <li>Farms</li>
+            <li>Beach</li>
+            <li>Learn more</li>
+          </ul>
+        </div>
+
+        <div >
+          <h5>About us</h5>
+          <ul>
+            <li>Road map</li>
+            <li>Creators</li>
+            <li>Career</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/places/Places.jsx
+++ src/components/places/Places.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import styles from '../places/Places.module.scss';
 import PlaceCard from '../placeCard/PlaceCard';
+import image from '../../styles/assets/setting.png';
 
 const Places = () => {
   return (
     <section className={styles.places}>
+        <div className={styles.places_filter}>
+          <ul>
+            <li>Restaurant</li>
+            <li>Cottage</li>
+            <li>Castle</li>
+            <li>fantasy city</li>
+            <li>beach</li>
+            <li>Cabins</li>
+            <li>Off-grid</li>
+            <li>Farm</li>
+          </ul>
+
+          <div className={styles.places_filter__btn}>
+            <span>Location</span>
+            <img src={image} alt="" />
+          </div>
+        </div>
 
         <div className={styles.places_card}>
-            <PlaceCard />
+          <PlaceCard />
         </div>
     </section>
   )
